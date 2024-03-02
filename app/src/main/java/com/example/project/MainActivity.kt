ed344396f6bdf732ab96c11e01f3a4d80047ad0b
Shannon Cui <s38cui@uwaterloo.ca>
2024-03-02 13:17:28 -0500

    add destination basic ui

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.example.project
-
-import TripConfigurationForm
+import AutoComplete
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.project.ui.theme.ProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,25 +19,29 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    TripConfigurationForm("create")
+                    AutoComplete()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProjectTheme {
-        Greeting("Android")
-    }
-}
+//class MainActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//
+//        setContent {
+//            ProjectTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    AddDestinationForm()
+//                }
+//            }
+//        }
+//    }
+//}
---
+++ app/src/main/java/com/example/project/views/AddDestination.kt
@@ -0,0 +1,212 @@
+
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.KeyboardArrowDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.toSize
+
+@Composable
+fun AutoComplete() {
+
+    val destinations = listOf(
+        "CN tower",
+        "Yonge Dundas Square",
+        "Ripleys Aquarium",
+        "Union Station",
+        "AGO Museum",
+        "Harbourfront",
+        "Kensington market",
+        "University of Toronto",
+        "Subway"
+    )
+
+
+    var category by remember {
+        mutableStateOf("")
+    }
+
+    val heightTextFields by remember {
+        mutableStateOf(55.dp)
+    }
+
+    var textFieldSize by remember {
+        mutableStateOf(Size.Zero)
+    }
+
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+    val interactionSource = remember {
+        MutableInteractionSource()
+    }
+
+    // Category Field
+    Column(
+        modifier = Modifier
+            .padding(30.dp)
+            .fillMaxWidth()
+            .clickable(
+                interactionSource = interactionSource,
+                indication = null,
+                onClick = {
+                    expanded = false
+                }
+            )
+    ) {
+
+        Text(
+            modifier = Modifier.padding(start = 3.dp, bottom = 2.dp),
+            text = "Add Destinations",
+            fontSize = 16.sp,
+            color = Color.Black,
+            fontWeight = FontWeight.Medium,
+            onTextLayout = {}
+        )
+
+        Column(modifier = Modifier.fillMaxWidth()) {
+
+            Row(modifier = Modifier.fillMaxWidth()) {
+                TextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(heightTextFields)
+                        .border(
+                            width = 1.8.dp,
+                            color = Color.Black,
+                            shape = RoundedCornerShape(10.dp)
+                        )
+                        .onGloballyPositioned { coordinates ->
+                            textFieldSize = coordinates.size.toSize()
+                        },
+                    value = category,
+                    onValueChange = {
+                        category = it
+                        expanded = true
+                    },
+                    placeholder = { Text("Enter a Destination",
+                        onTextLayout = {}) },
+                    textStyle = TextStyle(
+                        color = Color.Black,
+                        fontSize = 16.sp
+                    ),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Done
+                    ),
+                    singleLine = true,
+                    trailingIcon = {
+                        IconButton(onClick = { expanded = !expanded }) {
+                            Icon(
+                                modifier = Modifier.size(24.dp),
+                                imageVector = Icons.Rounded.KeyboardArrowDown,
+                                contentDescription = "arrow",
+                                tint = Color.Black
+                            )
+                        }
+                    }
+                )
+            }
+
+            AnimatedVisibility(visible = expanded) {
+                Card(
+                    modifier = Modifier
+                        .padding(horizontal = 5.dp)
+                        .width(textFieldSize.width.dp),
+                    shape = RoundedCornerShape(10.dp)
+                ) {
+
+                    LazyColumn(
+                        modifier = Modifier.heightIn(max = 150.dp),
+                    ) {
+
+                        if (category.isNotEmpty()) {
+                            items(
+                                destinations.filter {
+                                    it.lowercase()
+                                        .contains(category.lowercase()) || it.lowercase()
+                                        .contains("others")
+                                }
+                                    .sorted()
+                            ) {
+                                ItemsCategory(title = it) { title ->
+                                    category = title
+                                    expanded = false
+                                }
+                            }
+                        } else {
+                            items(
+                                destinations.sorted()
+                            ) {
+                                ItemsCategory(title = it) { title ->
+                                    category = title
+                                    expanded = false
+                                }
+                            }
+                        }
+
+                    }
+
+                }
+            }
+        }
+        Button(onClick = {}) {
+            Text("Add")
+        }
+    }
+}
+
+@Composable
+fun ItemsCategory(
+    title: String,
+    onSelect: (String) -> Unit
+) {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                onSelect(title)
+            }
+            .padding(10.dp)
+    ) {
+        Text(text = title, fontSize = 16.sp,
+            onTextLayout = {})
+    }
+
+}
