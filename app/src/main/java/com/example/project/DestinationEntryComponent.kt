ae1793b6dc607b7ef5bbf593f501e8f6f8a12129
Derrick Yang <yangguangzheng122@gmail.com>
2024-03-03 17:58:20 -0500

    Updated destination entry component and completed the top half of voting results page

--- app/src/main/java/com/example/project/DestinationEntryComponent.kt
+++ app/src/main/java/com/example/project/DestinationEntryComponent.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -15,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -43,24 +45,20 @@ fun DestinationEntry(destination: Destination) {
         ),
         shape = RoundedCornerShape(15.dp),
         modifier = Modifier
-            .padding(horizontal = 16.dp, vertical = 8.dp)
             .fillMaxWidth()
 
     ) {
-        Row(
+        Row (
             verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(16.dp)
-        ) {
-//            NumberIndicator(number = number, destination.voteCount)
-
-            Spacer(modifier = Modifier.width(16.dp))
-
+            modifier = Modifier.padding(10.dp).fillMaxWidth()
+        ){
             Column {
                 Text(
                     text = destination.name,
                     fontWeight = FontWeight.Bold,
                     fontSize = 18.sp
                 )
+                Spacer(modifier = Modifier.height(5.dp))
                 Row(verticalAlignment = Alignment.CenterVertically) {
 //                    RatingBar(rating = destination.rating.toFloat()) // Assuming you have a custom RatingBar composable
                     Image(
@@ -70,10 +68,10 @@ fun DestinationEntry(destination: Destination) {
                     )
                     Text(
                         text = " ${destination.address}",
-                        color = Color.Gray,
                         fontSize = 14.sp
                     )
                 }
+                Spacer(modifier = Modifier.height(5.dp))
                 Row(verticalAlignment = Alignment.CenterVertically) {
 //                    RatingBar(rating = destination.rating.toFloat()) // Assuming you have a custom RatingBar composable
                     Image(
@@ -86,20 +84,22 @@ fun DestinationEntry(destination: Destination) {
                     )
                     Text(
                         text = "(${destination.reviewCount})",
-                        color = Color.Gray,
                         fontSize = 14.sp
                     )
                 }
             }
-//            Spacer(modifier = Modifier.weight(1f))
-//
-//            Image(
-//                painter = painterResource(id = destination.imageResId),
-//                contentDescription = "${destination.name} image",
-//                modifier = Modifier
-//                    .size(80.dp)
-//                    .clip(RoundedCornerShape(8.dp))
-//            )
+
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Image(
+                painter = painterResource(id = destination.imageResId),
+                contentDescription = "${destination.name} image",
+                modifier = Modifier
+                    .size(65.dp)
+                    .clip(RoundedCornerShape(8.dp))
+            )
         }
     }
 }
+
