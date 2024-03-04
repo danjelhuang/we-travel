e9e08b0ee1b583af0850eb902f28b0048b0778f5
Jesse Liu <jesseliu80@gmail.com>
2024-03-03 19:03:21 -0500

    Bottom card

--- app/src/main/java/com/example/project/DestinationsList.kt
+++ app/src/main/java/com/example/project/DestinationsList.kt
@@ -2,6 +2,7 @@ package com.example.project
 
 import android.text.style.ClickableSpan
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -17,6 +18,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Scaffold
 
 import androidx.compose.material3.Text
@@ -138,12 +145,89 @@ fun DestinationsColumn(destinations: List<Destination>, innerPadding: PaddingVal
     LazyColumn (
         modifier = Modifier
             .padding(innerPadding),
-        verticalArrangement = Arrangement.spacedBy(0.dp),
     ) {
         itemsIndexed(destinations) { _, destination ->
-            DestinationEntry(destination = destination)
+            Row(
+                modifier = Modifier.padding(vertical = 8.dp, horizontal = 10.dp)
+            ){
+                DestinationEntry(destination = destination)
+            }
+
         }
     }
+}
+
+
+@Composable
+fun BottomCard() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Color(0xFFA8DADC))
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(vertical = 24.dp, horizontal = 16.dp)
+        ) {
+
+            // Voting countdown
+            Column {
+                Text(
+                    text = "Voting begins in...",
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp,
+                        textAlign = TextAlign.Center,
+                        color = Color(0xFFFFFFFF)
+                    )
+                )
+                Text(
+                    text = "1d 14hrs",
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 40.sp,
+                        textAlign = TextAlign.Center,
+                        color =  Color(0xFFE63946)
+                    )
+                )
+            }
+
+            Spacer(modifier = Modifier.width(20.dp))
+            
+            // TODO: Move this out
+            val mainButtonColor = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFE63946),
+                contentColor = Color.White
+            )
+
+            // Add destination button
+            FilledTonalButton(
+                onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(20),
+                colors = mainButtonColor,
+                contentPadding = PaddingValues(horizontal = 10.dp, vertical = 8.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.add_square),
+                    contentDescription = "User icon",
+                    modifier = Modifier.size(32.dp)
+                )
+                Spacer(modifier = Modifier.width(10.dp))
+                Text(
+                    text = "Destination",
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp,
+                    )
+                )
+                Spacer(modifier = Modifier.width(10.dp))
+            }
+
+        }
+
+    }
+
 }
 
 
@@ -153,14 +237,15 @@ fun DestinationsList() {
     // Assuming we have a list of destinations to display
     val destinations = listOf(
         // Add your destinations here, for example:
-        Destination(1, "MoMA", "11 W 53rd St, New York", "4.6", 50, 5),
+        Destination(1, "MoMA", "11 W 53rd St, New York", "4.6", 50, R.drawable.sample_destination_image),
         // Add more destinations...
-        Destination(2, "MoMA", "11 W 53rd St, New York", "4.6", 50, 5),
-        Destination(3, "MoMA", "11 W 53rd St, New York", "4.6", 50, 5),
+        Destination(2, "MoMA", "11 W 53rd St, New York", "4.6", 50, R.drawable.sample_destination_image),
+        Destination(3, "MoMA", "11 W 53rd St, New York", "4.6", 50, R.drawable.sample_destination_image),
     )
 
     Scaffold(
-        topBar = { DestinationsListHeader(tripName = "Toronto") }
+        topBar = { DestinationsListHeader(tripName = "Toronto") },
+        bottomBar = { BottomCard() }
     ) { innerPadding ->
         DestinationsColumn(destinations = destinations, innerPadding = innerPadding)
     }
