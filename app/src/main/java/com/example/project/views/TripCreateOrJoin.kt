7e5ce59b25612f85345981ebe8bcac9b85a0e9a2
fionz <fionzheng0@gmail.com>
2024-03-03 23:26:41 -0500

    frontend for login/trip selection, updated font, colour and theme, uploaded logo

---
+++ app/src/main/java/com/example/project/views/TripCreateOrJoin.kt
@@ -0,0 +1,106 @@
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.project.R
+
+@Composable
+fun TripCreateorJoin() {
+    Column {
+        TopBar()
+        SelectionButtons()
+    }
+}
+@Composable
+@Preview
+fun SelectionButtons() {
+    val dmSansFamily = FontFamily(
+        Font(
+            resId = R.font.dmsans_semibold, FontWeight(600)
+        )
+    )
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(30.dp),
+    ) {
+
+        Button(
+            onClick = { /* Handle to navigate to the Create Trip Activity */ },
+            shape = RoundedCornerShape(12.dp),
+            modifier = Modifier
+                .height(125.dp)
+                .width(2000.dp)
+                .padding(16.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondary)
+        ) {
+            Text(text = "create trip", fontFamily = dmSansFamily, fontSize = 36.sp /*color = Color.White*/)
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { /* Handle to navigate to the Join Trip Activity */ },
+            shape = RoundedCornerShape(12.dp),
+            modifier = Modifier
+                .height(125.dp)
+                .width(2000.dp)
+                .padding(16.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondary)
+        ) {
+            Text(text = "join trip", fontFamily = dmSansFamily, fontSize = 36.sp /*color = Color.White*/)
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar() {
+    TopAppBar(
+        title = {},
+        navigationIcon = {
+            Image(
+                painter = painterResource(id = R.drawable.logo_we),
+                contentDescription = "WeTravelLogo",
+                modifier = Modifier.size(100.dp)
+            )
+        },
+        modifier = Modifier.fillMaxWidth(),
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.background
+        )
+    )
+}
+
+
+
+
+
+
