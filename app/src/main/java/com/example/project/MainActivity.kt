52d778a9d84c0151316eadc46660e08e03579144
Shannon Cui <s38cui@uwaterloo.ca>
2024-03-04 20:29:11 -0500

    add destination basic ui updated

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -25,23 +25,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-
-//class MainActivity : ComponentActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//
-//
-//        setContent {
-//            ProjectTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//                    AddDestinationForm()
-//                }
-//            }
-//        }
-//    }
-//}
