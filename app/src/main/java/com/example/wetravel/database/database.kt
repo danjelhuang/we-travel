e37c896e3926ad3f834e9ff15c3dbdcf720ca940
dhuang <daniel9huang@gmail.com>
2024-03-20 19:32:13 -0400

    implement database for node

--- app/src/main/java/com/example/wetravel/database/database.kt
+++
@@ -1,9 +0,0 @@
-package com.example.wetravel.database
-
-import com.google.firebase.firestore.FirebaseFirestore
-
-object DatabaseManager {
-    val db: FirebaseFirestore by lazy {
-        FirebaseFirestore.getInstance()
-    }
-}
