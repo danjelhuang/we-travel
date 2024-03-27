7a9e9bde6b781a76d2e51ad16ec131b2c69e86ec
Shannon Cui <shannon.cui@uwaterloo.ca>
2024-03-27 18:15:01 -0400

    updating grade files

--- frontend-android/build.gradle.kts
+++ frontend-android/build.gradle.kts
@@ -4,20 +4,3 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
 }
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-
-    dependencies {
-        classpath("com.android.tools.build:gradle:7.1.3")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-        classpath("com.google.gms:google-services:4.3.10")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
-    }
-
-
-
-
-}
