e03fb39dd159426879fcf91959fc7588ca3a58aa
dhuang <daniel9huang@gmail.com>
2024-03-31 17:30:44 -0400

    remove field on trip config

--- frontend-android/app/src/main/java/com/example/wetravel/views/TripConfiguration.kt
+++ frontend-android/app/src/main/java/com/example/wetravel/views/TripConfiguration.kt
@@ -1,23 +1,17 @@
-import android.util.Log
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Switch
-import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -49,7 +43,6 @@ fun TripConfigurationForm(
     val destinationCity = remember { mutableStateOf(TextFieldValue()) }
     val finalDestinationCount = remember { mutableStateOf(TextFieldValue()) }
     val numberOfVotesPerPerson = remember { mutableStateOf(TextFieldValue()) }
-    val allowAnonymousVoting = remember { mutableStateOf(false) }
     val buttonText = if (title == "create") "create" else "save changes"
     val dmSansFamily = FontFamily(
         Font(
@@ -97,35 +90,7 @@ fun TripConfigurationForm(
             onValueChange = { finalDestinationCount.value = it },
             dmSansFamily
         )
-        Spacer(modifier = Modifier.height(20.dp))
-        InputField(
-            label = "number of votes/person",
-            value = numberOfVotesPerPerson.value,
-            onValueChange = { numberOfVotesPerPerson.value = it },
-            dmSansFamily
-        )
-        Spacer(modifier = Modifier.height(20.dp))
-
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Switch(
-                colors = SwitchDefaults.colors(
-                    checkedTrackColor = MaterialTheme.colorScheme.secondary
-                ),
-                checked = allowAnonymousVoting.value,
-                onCheckedChange = { allowAnonymousVoting.value = it }
-            )
-            Spacer(modifier = Modifier.width(16.dp))
-            Text(
-                "allow anonymous voting",
-                color = MaterialTheme.colorScheme.primary,
-                fontFamily = dmSansFamily
-            )
-        }
-
+        Spacer(modifier = Modifier.height(40.dp))
         Spacer(modifier = Modifier.height(16.dp))
 
         Button(
@@ -133,7 +98,6 @@ fun TripConfigurationForm(
                 handleButtonClick(
                     tripName.value.text,
                     destinationCity.value.text,
-                    numberOfVotesPerPerson.value.text,
                     finalDestinationCount.value.text,
                     userViewModel = userViewModel
                 )
@@ -195,14 +159,12 @@ private fun handleButtonClick(
     tripName: String,
     destinationCity: String,
     finalDestinationCount: String,
-    numberOfVotesPerPerson: String,
     userViewModel: UserViewModel
 ) {
     val tripData = Trip(
         name = tripName,
         city = destinationCity,
-        finalDestinationCount = finalDestinationCount.toInt(),
-        votesPerPerson = finalDestinationCount.toInt()
+        finalDestinationCount = finalDestinationCount.toInt()
     )
     userViewModel.createTrip(tripData)
 }
