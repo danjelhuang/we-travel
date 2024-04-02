c39d803c4865890519a0aea95f83cfd6176f72fb
Derrick Yang <yangguangzheng122@gmail.com>
2024-04-01 23:28:43 -0400

    Added join trip fe integration

--- frontend-android/app/src/main/java/com/example/wetravel/views/DestinationsList.kt
+++ frontend-android/app/src/main/java/com/example/wetravel/views/DestinationsList.kt
@@ -1,5 +1,6 @@
 package com.example.wetravel.views
 
+import android.util.Log
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -7,6 +8,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.wetravel.R
@@ -14,6 +17,10 @@ import com.example.wetravel.components.DestinationEntry
 import com.example.wetravel.components.DestinationsListFooter
 import com.example.wetravel.components.DestinationsListHeader
 import com.example.wetravel.models.Destination
+import com.example.wetravel.models.Resource
+import com.example.wetravel.models.Trip
+import com.example.wetravel.models.User
+import com.example.wetravel.models.UserViewModel
 import java.util.UUID
 
 @Composable
@@ -37,7 +44,8 @@ fun DestinationsColumn(destinations: List<Destination>, innerPadding: PaddingVal
 fun DestinationsList(
     onAddDestinationButtonClicked: () -> Unit,
     onStartVotingButtonClicked: () -> Unit,
-    onSettingsButtonClicked: () -> Unit
+    onSettingsButtonClicked: () -> Unit,
+    userViewModel: UserViewModel
 ) {
     // Assuming we have a list of destinations to display
     val destinations = listOf(
@@ -56,6 +64,16 @@ fun DestinationsList(
             R.drawable.sample_destination_image, voted = true, numOfVotes = 5, description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
         ),
     )
+
+//    val tripCodeResource by userViewModel.tripCode.observeAsState(initial = Resource.Loading)
+//    val allTrips by userViewModel.allTrips.observeAsState(initial = Resource.Loading)
+//    val user by userViewModel.user.observeAsState(initial = Resource.Loading)
+//
+//    if (tripCodeResource is Resource.Success && allTrips is Resource.Success && user is Resource.Success) {
+//        Log.d("Frontend state after Join", (tripCodeResource as Resource.Success<String>).data)
+//        Log.d("Frontend state after Join", (allTrips as Resource.Success<Map<String, Trip>>).data.toString())
+//        Log.d("Frontend state after Join", (user as Resource.Success<User>).data.toString())
+//    }
 
     Scaffold(
         topBar = { DestinationsListHeader(tripName = "Toronto", onSettingsButtonClicked) },
