a3a600047dea5ad62f55695487a03971f8cb07a0
Jerry Cheng <j983chen@uwaterloo.ca>
2024-04-02 21:03:58 -0400

    PROFILE PAGE UPDATES WOOOO

--- frontend-android/app/src/main/java/com/example/wetravel/views/DestinationsList.kt
+++ frontend-android/app/src/main/java/com/example/wetravel/views/DestinationsList.kt
@@ -1,8 +1,10 @@
 package com.example.wetravel.views
 
-import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -12,7 +14,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.wetravel.components.DestinationEntry
 import com.example.wetravel.components.DestinationsListFooter
@@ -22,10 +26,11 @@ import com.example.wetravel.models.Resource
 import com.example.wetravel.models.Trip
 import com.example.wetravel.models.User
 import com.example.wetravel.models.UserViewModel
-import java.util.UUID
 
 @Composable
-fun DestinationsColumn(destinations: List<Destination>, innerPadding: PaddingValues) {
+fun DestinationsColumn(destinations: List<Destination>,
+                       innerPadding: PaddingValues,
+                       userViewModel: UserViewModel) {
     LazyColumn(
         modifier = Modifier
             .padding(innerPadding),
@@ -46,9 +51,10 @@ fun DestinationsList(
     onAddDestinationButtonClicked: () -> Unit,
     onStartVotingButtonClicked: () -> Unit,
     onSettingsButtonClicked: () -> Unit,
-    userViewModel: UserViewModel
+    userViewModel: UserViewModel,
+    userName: String? = ""
 ) {
-    val sampleTrip by userViewModel.sampleTrip.observeAsState(initial = Resource.Loading)
+    val sampleTripResource by userViewModel.sampleTrip.observeAsState(initial = Resource.Loading)
     // Assuming we have a list of destinations to display
 //    val destinations = listOf(
 //        // Add your destinations here, for example:
@@ -67,37 +73,72 @@ fun DestinationsList(
 //        ),
 //    )
 
-//    val tripCodeResource by userViewModel.tripCode.observeAsState(initial = Resource.Loading)
-//    val allTrips by userViewModel.allTrips.observeAsState(initial = Resource.Loading)
-//    val user by userViewModel.user.observeAsState(initial = Resource.Loading)
-//
-//    if (tripCodeResource is Resource.Success && allTrips is Resource.Success && user is Resource.Success) {
-//        Log.d("Frontend state after Join", (tripCodeResource as Resource.Success<String>).data)
-//        Log.d("Frontend state after Join", (allTrips as Resource.Success<Map<String, Trip>>).data.toString())
-//        Log.d("Frontend state after Join", (user as Resource.Success<User>).data.toString())
-//    }
+    val tripCodeResource by userViewModel.tripCode.observeAsState(initial = Resource.Loading)
+    val tripsResource by userViewModel.allTrips.observeAsState(initial = Resource.Loading)
+    val userResource by userViewModel.user.observeAsState(initial = Resource.Loading)
 
-    Scaffold(
-        topBar = { DestinationsListHeader(tripName = "Toronto", onSettingsButtonClicked) },
-        bottomBar = { DestinationsListFooter(onAddDestinationButtonClicked, onStartVotingButtonClicked, userViewModel) }
-    ) { innerPadding ->
-        when (sampleTrip) {
-            is Resource.Success -> {
-                val trip = (sampleTrip as Resource.Success<Trip>).data
-                DestinationsColumn(destinations = trip.destinationsList, innerPadding = innerPadding)
-            }
-            is Resource.Loading -> {
-                CircularProgressIndicator()
-            }
-            is Resource.Error -> {
-                val errorMessage = (sampleTrip as Resource.Error).message
-                // Display the error message
-                Text(text = "Error: $errorMessage")
+//    Scaffold(
+//        topBar = { DestinationsListHeader(tripName = "Toronto", onSettingsButtonClicked) },
+//        bottomBar = { DestinationsListFooter(onAddDestinationButtonClicked, onStartVotingButtonClicked) }
+//    ) { innerPadding ->
+//        when (sampleTrip) {
+//            is Resource.Success -> {
+//                val trip = (sampleTrip as Resource.Success<Trip>).data
+//                DestinationsColumn(destinations = trip.destinationsList, innerPadding = innerPadding)
+//            }
+//            is Resource.Loading -> {
+//                CircularProgressIndicator()
+//            }
+//            is Resource.Error -> {
+//                val errorMessage = (sampleTrip as Resource.Error).message
+//                // Display the error message
+//                Text(text = "Error: $errorMessage")
+//            }
+//        }
+
+    when {
+        tripCodeResource is Resource.Success<*> && tripsResource is Resource.Success<*> && userResource is Resource.Success<*> -> {
+            val tripCode = (tripCodeResource as Resource.Success<String>).data
+            val trip = (tripsResource as Resource.Success<Map<String, Trip>>).data[tripCode]
+            val user = (userResource as Resource.Success<User>).data
+
+            Scaffold(
+                topBar = { DestinationsListHeader(
+                    tripName = trip?.name ?: "",
+                    userName = userName ?: "User",
+                    numParticipants = trip?.users?.size ?: -1,
+                    onSettingsButtonClicked = onSettingsButtonClicked) },
+                bottomBar = { DestinationsListFooter(onAddDestinationButtonClicked, onStartVotingButtonClicked, userViewModel) }
+            ) { innerPadding ->
+                val sampleTrip = (sampleTripResource as Resource.Success<Trip>).data
+                DestinationsColumn(destinations = sampleTrip.destinationsList, innerPadding = innerPadding, userViewModel = userViewModel)
             }
         }
 
+        tripCodeResource is Resource.Loading || tripsResource is Resource.Loading || userResource is Resource.Loading -> {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+        tripCodeResource is Resource.Error || tripsResource is Resource.Error || userResource is Resource.Error -> {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                val tripCodeErrorMessage = (tripCodeResource as Resource.Error).message
+                val tripsErrorMessage = (tripsResource as Resource.Error).message
+                val userErrorMessage = (userResource as Resource.Error).message
+                Text(text = "Error: $tripCodeErrorMessage, $tripsErrorMessage, $userErrorMessage", textAlign = TextAlign.Center)
+            }
+        }
     }
 }
+
 
 
 
